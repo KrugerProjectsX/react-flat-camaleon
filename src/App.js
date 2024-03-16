794cd72a05c51f0bbcefcf2f6845db280ebfc3ed
Morales Bryan <bmorales@krugerschool.edu.ec>
2024-03-15 20:01:49 -0400

    User Formal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AddFlat from "./pages/AddFlat";
+import Profile from "./pages/Profile";
 function App() {
   return (
    <Routes>
       <Route path={"/"} element={<Login/>}/>
       <Route path={"/dashboard"} element={<Home/>}/>
       <Route path={"/flat/new"} element={<AddFlat/>}/>
+      <Route path={"/profile"} element={<Profile/>}/>
    </Routes>
   );
 };
