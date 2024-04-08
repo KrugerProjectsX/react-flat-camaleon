b7d8545f5b7922e64581c186ececf9d7867212ea
Morales Bryan <152868991+bmoralesKrugerschool@users.noreply.github.com>
2024-04-08 06:56:39 -0500

    filtros

--- src/components/FlatsTable.js
+++ src/components/FlatsTable.js
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import { useEffect, useState } from "react";
 import {getDocs, query, where, collection, addDoc, doc, deleteDoc, getDoc} from "firebase/firestore";
 import { db } from "../firebase";
-import {Button} from "@mui/material";
+import {Button,TextField,Slider} from "@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import EditIcon from '@mui/icons-material/Edit';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
@@ -22,6 +22,11 @@ export default function FlatsTable({ type }) {
     const userId = JSON.parse(localStorage.getItem('user_logged'));
     const [flats, setFlats] = useState([]);
     const [flag, setFlag ] = useState(false);
+    const [citySearch, setCitySearch] = useState('');
+    const [minArea, setMinArea] = useState(''); // Estado para almacenar el área mínima del rango
+    const [maxArea, setMaxArea] = useState(''); // Estado para almacenar el área máxima del rango
+    const [rentPriceRange, setRentPriceRange] = useState([0, 1000]); // Rango de precios de alquiler
+
 
     const getData = async () => {
         if (type === 'my-flats') {
@@ -85,8 +90,95 @@ export default function FlatsTable({ type }) {
         getData();
     }, [flag]);
 
+     // Función para manejar la búsqueda por ciudad
+     const handleCitySearch = async () => {
+        const searchQuery = query(ref, where("city", "==", citySearch));
+        const data = await getDocs(searchQuery);
+        const rows = data.docs.map((item) => {
+            return { ...item.data(), id: item.id }
+        });
+        setFlats(rows);
+    };
+    // Función para manejar la búsqueda por rango de área
+    const handleAreaSearch = async () => {
+        // Convertir los valores de minArea y maxArea a números
+        const minAreaNumber = parseFloat(minArea);
+        const maxAreaNumber = parseFloat(maxArea);
+
+        // Verificar si los valores son válidos y realizar la búsqueda
+        if (!isNaN(minAreaNumber) && !isNaN(maxAreaNumber)) {
+            const searchQuery = query(ref, where("areaSize", ">=", minAreaNumber), where("areaSize", "<=", maxAreaNumber));
+            const data = await getDocs(searchQuery);
+            const rows = data.docs.map((item) => {
+                return { ...item.data(), id: item.id }
+            });
+            setFlats(rows);
+        } else {
+            // Mostrar un mensaje de error si los valores no son válidos
+            alert('Please enter valid area size range.');
+        }
+    };
+    // Función para manejar el cambio en el rango de precios de alquiler
+    const handleRentPriceChange = (event, newValue) => {
+        setRentPriceRange(newValue);
+    };
+
+    // Función para realizar la búsqueda por rango de precios de alquiler
+    const searchByRentPriceRange = async () => {
+        const minPrice = rentPriceRange[0];
+        const maxPrice = rentPriceRange[1];
+        
+        const searchQuery = query(ref, where("rentPrice", ">=", minPrice), where("rentPrice", "<=", maxPrice));
+        const data = await getDocs(searchQuery);
+        const rows = data.docs.map((item) => {
+            return { ...item.data(), id: item.id }
+        });
+        setFlats(rows);
+    };
+
     return (
         <TableContainer>
+            <div style={{ margin: '20px' }}>
+                <Slider
+                    value={rentPriceRange}
+                    onChange={handleRentPriceChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    min={0}
+                    max={900}
+                />
+                {/* Botón para iniciar la búsqueda por rango de precios */}
+                <Button variant="contained" onClick={searchByRentPriceRange} style={{ marginTop: '10px' }}>Search by Rent Price</Button>
+            </div>
+            <TextField
+                label="Search by City"
+                variant="outlined"
+                value={citySearch}
+                onChange={(e) => setCitySearch(e.target.value)}
+                style={{ margin: '20px 0' }}
+                // Agregar un botón de búsqueda para iniciar la búsqueda
+                InputProps={{
+                    endAdornment: (
+                        <Button variant="contained" onClick={handleCitySearch}>Search</Button>
+                    )
+                }}
+            />
+            <TextField
+                label="Min Area Size"
+                variant="outlined"
+                value={minArea}
+                onChange={(e) => setMinArea(e.target.value)}
+                style={{ margin: '10px' }}
+            />
+            <TextField
+                label="Max Area Size"
+                variant="outlined"
+                value={maxArea}
+                onChange={(e) => setMaxArea(e.target.value)}
+                style={{ margin: '10px' }}
+            />
+            {/* Agregar un botón de búsqueda para iniciar la búsqueda por área */}
+            <Button variant="contained" onClick={handleAreaSearch} style={{ margin: '10px' }}>Search by Area</Button>
             <Table className="min-w-full divide-y divide-gray-200" aria-label="simple table">
                 <TableHead className="bg-gray-50">
                     <TableRow>
